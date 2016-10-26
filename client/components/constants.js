f8a40d95db5ca67064fcfcfa37c5b8f0ce2325a3
altose87 <altose87@gmail.com>
2016-10-25 21:10:57 -0500

    use heroku as server

--- client/components/constants.js
+++ client/components/constants.js
@@ -2,8 +2,8 @@ import FluxConstant from 'flux-constants'
 
 var AutobeepConstants = {
     API: {
-    	// API_URL: 'https://autobeep.herokuapp.com/api/v1/'
-        API_URL: 'http://dev.updatedcar.com/api/v1/'
+    	API_URL: 'https://autobeep.herokuapp.com/api/v1/'
+        // API_URL: 'http://dev.updatedcar.com/api/v1/'
     },
     AUTH: {
     	token: ''
